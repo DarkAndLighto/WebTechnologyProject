68151fc1b7ff8d56ef0522e4c9e9a529f89e61f0
DarkAndLighto <ammarajam08@gmail.com>
2024-04-14 22:26:12 +0300

    Day 7 (PHP testing)
    php testing

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
 <?php
-echo "Hello maties";
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $con = new mysqli("localhost", "root", "", "webproject");
+    if ($con->connect_error)
+    {
+        die("Failed to connect: " .$con->connect_error);
+    } else
+    {
+        $stmt = $con->prepare("select * from projectdatabase where email = ?");
+        $stmt -> bind_param("s", $email);
+        $stmt -> execute();
+        $stmt_result = $stmt->get_result();
+        if ($stmt_result -> num_rows > 0)
+        {
+            $data = $stmt_result->fetch_assoc();
+            if ($data['password'] === $password)
+            {
+                echo "<script>alert('Login Successful');</script>";
+            } else
+            {
+                echo '<h2>Login Failed</h2>';
+            }
+        }
+    }
 ?>
