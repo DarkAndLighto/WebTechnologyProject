1b91038a58bcad12566f8544f4e9d8eea4200d59
DarkAndLighto <ammarajam08@gmail.com>
2024-04-15 05:15:32 +0300

    Day 7
    Major changes:
    -Using 000webhosting for hosting the website instead of GitHub because it doesn't support PHP.
    -Added login page (no practical functionality)
    -Reorganized the files.

--- login.php
+++ login.php
@@ -2,13 +2,16 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $con = new mysqli("localhost", "root", "", "webproject");
+    //echo $email;
+    //echo $password;
+    /* THIS WILL --NOT-- WORK WITH WAMP */
+    $con = new mysqli("localhost", "id22019662_ammar", "Greenberrypie1!", "id22019662_projectdatabase");
     if ($con->connect_error)
     {
         die("Failed to connect: " .$con->connect_error);
     } else
     {
-        $stmt = $con->prepare("select * from projectdatabase where email = ?");
+        $stmt = $con->prepare("select * from ProjectDatabase where email = ?");
         $stmt -> bind_param("s", $email);
         $stmt -> execute();
         $stmt_result = $stmt->get_result();
@@ -17,11 +20,48 @@
             $data = $stmt_result->fetch_assoc();
             if ($data['password'] === $password)
             {
-                echo "<script>alert('Login Successful');</script>";
+                echo "Password is CORRECT and it is $password";
+                echo "<script>alert('Login Successful. Welcome $email!');</script>";
             } else
             {
-                echo '<h2>Login Failed</h2>';
+                echo "Password is WRONG and it is $password";
+                echo "<script>alert('Login Failed! Please try again.');</script>";
+                header("Location: login.html");
             }
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+  <link href="styling.css" rel="stylesheet" />
+  <link href="login.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <div class="container"> 
+        <div class="blurry-box return-home">
+            <a href="index.html">
+                <button>
+                    Return home
+                </button>
+            </a>
+        </div>
+    </div>
+  
+  <script src="https://kit.fontawesome.com/41752380dd.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
