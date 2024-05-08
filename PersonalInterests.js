2737a21779de0f79c18c09e94586f095af944692
DarkAndLighto <ammarajam08@gmail.com>
2024-05-08 06:36:31 +0300

    Day 12
    -Finished the my interests page.

--- PersonalInterests.js
+++ PersonalInterests.js
@@ -1,4 +1,13 @@
-function GetGameByID(id, boxID) {
+function GetGameByIDWrapper(boxid) {
+    var textarea = document.getElementById('getMyId');
+    var id = textarea.value;
+    GetGameByID(id, boxid);
+}
+
+function GetGameByID(id, boxid, includeDeleteButton = true) {
+    var textarea = document.getElementById('getMyId');
+    textarea.value = "";
+
     const key = '51f03d61ff3442008e9b1aac81f4ca38';
     const url = `https://api.rawg.io/api/games/${id}?key=${key}`;
 
@@ -8,26 +17,98 @@ function GetGameByID(id, boxID) {
     };
 
     fetch(`${url}`, requestOptions)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.text())
+    .then((result) => {
+        const data = JSON.parse(result);
         const gameName = data.name;
-        const gameNameDiv = document.querySelector('.game-name-'+boxID);
-        if (gameNameDiv) {
-            gameNameDiv.innerText = gameName;
-        } else {
-            console.error('Game name element not found');
-        }
-
         const backgroundImage = data.background_image;
-        const backgroundImageDiv = document.querySelector('.game-background-'+boxID);
-            backgroundImageDiv.src = backgroundImage;
+        const releaseDate = data.released;
+        const rating = data.rating;
+        const genreNames = data.genres.map(genre => " " +genre.name);
+        const description = data.description_raw;
+        addNewBox(gameName, backgroundImage, releaseDate, rating, genreNames, description, includeDeleteButton, boxid)
     })
     .catch(error => console.error(error));
 }
+/* GetGameByID(422, 1, false);
+GetGameByID(19345, 1, false);
+GetGameByID(50734, 1, false);
+ */
+const box = (name, image, releaseDate, rating, genre, description, includeDeleteButton = true) => {
+    let deleteButtonHtml = '';
+    if (includeDeleteButton) {
+        deleteButtonHtml = `
+            <div class="row-delete">
+                <button onclick="deleteBox(event)">Delete box</button>
+            </div>
+        `;
+    }
 
+    return `<div class="col-lg-12 col-xl-4 mark-for-delete">
+                <div class="blurry-box game-box" style="color: white;">
+                    <div class="row-1">
+                        <img src="${image}" class="img-fluid game-background-1">
+                    </div>
+                    <div class="row-2">
+                        <div class="game-name-1">
+                            ${name}
+                        </div>
+                    </div>
+                    <div class="decorative-line"></div>
+                    <div class="row-3">
+                        <div class="row">
+                            <div class="col-6">
+                                Release Date:
+                            </div>
+                            <div class="col-6 release-date right-side">
+                                ${releaseDate}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                Rating:
+                            </div>
+                            <div class="col-6 rating right-side">
+                                ${rating}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                Genre:
+                            </div>
+                            <div class="col-6 game-genre right-side">
+                                ${genre}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="decorative-line"></div>
+                    <div class="row-4">
+                        <textarea readonly>${description}</textarea>
+                    </div>
+                    ${deleteButtonHtml}
+                </div>
+            </div>`;
+}
 
+const addNewBox = (name, image, releaseDate, rating, genre, description, includeDeleteButton, boxid) => {
+    const container = document.querySelector('.boxes-'+boxid);
+    container.innerHTML += box(name, image, releaseDate, genre, rating, description, includeDeleteButton);
+}
+document.addEventListener('click', function(e){
 
-
+});
+const deleteBox = (event) => {
+    var currentElement = event.target;
+    var customSelector = '.mark-for-delete';
+    
+    while (currentElement) {
+        if (currentElement.matches && currentElement.matches(customSelector)) {
+            currentElement.remove();
+            return;
+        }
+        currentElement = currentElement.parentNode;
+    }
+}
 
 
 
@@ -139,3 +220,4 @@ function printId(){
     </div>
 </div> 
 */
+
